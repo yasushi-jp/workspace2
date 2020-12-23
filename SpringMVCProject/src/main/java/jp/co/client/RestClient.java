466ddfbeaa842e48f48683499cb7f05121370495
大塚　泰司 <yasushi-ohtsuka@nifty.ne.jp>
2020-12-23 18:49:28 +0900

    サンプル追加 pom.xmlも修正

---
+++ SpringMVCProject/src/main/java/jp/co/client/RestClient.java
@@ -0,0 +1,55 @@
+package jp.co.client;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import jp.co.bean.ResData;
+
+public class RestClient {
+
+	public static void main(String[] args) {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		MultiValueMap<String, String> params= new LinkedMultiValueMap<String, String>();
+		params.add("page", "1");
+		params.add("per_page", "100");
+
+		URI uri = null;
+		URI queryURI = null;
+		try {
+			uri = new URI("https://qiita.com/api/v2/authenticated_user/items");
+			queryURI =  UriComponentsBuilder.fromUri(uri).queryParams(params).build().toUri();
+		} catch (URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		headers.add("Authorization", "Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+		RequestEntity<?> req = new RequestEntity<>(headers, HttpMethod.GET, queryURI);
+
+		ResponseEntity<ResData[]> res = restTemplate.exchange(req, ResData[].class);
+		System.out.println(res.getStatusCode());
+		ResData[] resData = res.getBody();
+
+		int cnt = 0;
+		for (ResData data : resData) {
+			cnt++;
+			System.out.println(cnt + ":" + data.getTitle());
+		}
+
+	}
+
+}
---
+++ SpringMVCProject/src/main/java/jp/co/client/RestClient1.java
@@ -0,0 +1,71 @@
+package jp.co.client;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jp.co.bean.ResData;
+
+public class RestClient1 {
+
+	public static void main(String[] args) {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+		params.add("page", "1");
+		params.add("per_page", "100");
+
+		String url = "https://qiita.com/api/v2/authenticated_user/items";
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url)
+				.queryParam("page", 1)
+				.queryParam("per_page", 100);
+
+		System.out.println(uriBuilder.build().encode().toUri());
+		ResponseEntity<String> response = restTemplate.exchange(
+				uriBuilder.build().encode().toUri(),
+				HttpMethod.GET,
+				entity,
+				String.class);
+
+		System.out.println(response.getStatusCode());
+		String json = response.getBody();
+		ObjectMapper mapper = new ObjectMapper();
+		ResData[] resData = null;
+		try {
+			resData = mapper.readValue(json, ResData[].class);
+		} catch (JsonParseException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		int cnt = 0;
+		for (ResData data : resData) {
+			cnt++;
+			System.out.println(cnt + ", title=" + data.getTitle());
+		}
+
+	}
+
+}
